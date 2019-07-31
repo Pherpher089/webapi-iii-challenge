fee0528cd46ede0b4ff0657cad15917929126348
Pherpher089 <Christutor089@gmail.com>
2019-07-31 01:30:51 -0700

    Finished mvp except for the put /api/user/:id/. Not really sure what this one is suposed to do

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
+//Apply middleware 
 const server = express();
+server.use(express.json());
+server.use(logger);
+//Apply routes 
+server.use('/api/user', userRouter);
+server.use('/api/post', postRouter);
 
+
+//Request Handeler
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.status(200).json({message: 'Lets write some express middleware!'})
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-
+  console.log(`Method Call: ${req.method} - URL: ${req.originalUrl} - Time Requested: ${+ new Date().getTime()/1000}`)
+  res.status(201)
+    next();
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,167 @@
-const express = 'express';
+const express = require('express');
+const db = require('./userDb.js');
+const postsDb = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, async (req, res) => {
+    const name = req.body.name;
+
+    if(!name) {
+        res.status(400).json({message: 'please provide a username.'})
+    }
+
+    try {
+        const newUser = await db.insert(req.body)
+        res.status(201).json({message: `User ${name} has been added to the database successfully.`});
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
+});
+
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
+    const id = req.params.id;
+    const text = req.body.text;
+
+    if(!id) {
+        res.status(401).json({errorMessage: 'Please provide the user id in the request path'});
+    }
+
+    if(!text) {
+        res.status(401).json({errorMessage: 'No text was provided for the post'})
+    }
+    
+    const post = {
+        text: req.body.text,
+        user_id: req.params.id
+    }
+
+    try {
+        const addedPost = await postsDb.insert(post);
+        res.status(201).json(addedPost);
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
 
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const users = await db.get();
+        res.status(200).json(users)
+    } catch ({message}) {
+        res.status(500).json(message)
+    }
 });
 
-router.get('/', (req, res) => {
+router.get('/:id', validateUserId, async (req, res) => {
+    const {id} = req.params;
+    if(!id) {
+        res.status(401).json({errorMessage: 'Please provide an id as parameter in the request route'})
+    }
 
+    try {
+        const user = await db.getById(id);
+        res.status(200).json(user)
+    } catch ({message}) {
+        res.status(500).json(message)
+    }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res) => {
+    const {id} = req.params;
+    if(!id) {
+        res.status(401).json({errorMessage: 'Please provide an id as parameter in the request route'})
+    }
 
+    try {
+        const user = await db.getUserPosts(id);
+        res.status(200).json(user)
+    } catch ({message}) {
+        res.status(500).json(message)
+    } 
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete('/:id', validateUserId, async (req, res) => {
+    const {id} = req.params;
+    if(!id) {
+        res.status(401).json({errorMessage: 'Please provide an id as parameter in the request route'})
+    }
 
+    try {
+        const user = await db.remove(id);
+        res.status(200).json(user)
+    } catch ({message}) {
+        res.status(500).json(message)
+    } 
 });
 
-router.delete('/:id', (req, res) => {
+router.put('/:id', validateUserId, async (req, res) => {
+    const id = req.params.id;
+    const changes = req.body.changes;
+    if(!id) {
+        res.status(401).json({errorMessage: 'Please provide an id as parameter in the request route'})
+    }
 
-});
+    if(!changes) {
+        res.status(401).json({errorMessage: 'No changes were provided'})
+    }
 
-router.put('/:id', (req, res) => {
-
-});
+    try {
+        const user = await db.update(req.params.id, req.body.changes);
+        res.status(200).json(user)
+    } catch ({message}) {
+        res.status(500).json(message)
+    } 
+})
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const id = req.params.id;
+    if(!id) {
+        res.status(400).json({errorMessage: 'No id was provided'})
+    }
 
+    db.getById(id)
+    .then(user => {
+        console.log('ValidateUserID is working');
+        res.user = user
+    })
+    .catch(err =>{
+        res.status(400).json({message: "invalid user id"})
+    })
+    
+    next();
 };
 
 function validateUser(req, res, next) {
+    const name = req.body.name;
 
+    if(!req.body) {
+        res.status(400).json({errorMessage: "Missing user data"})
+    } else if(!name) {
+        res.status(400).json({errorMessage: 'Missing required name field'})
+    } else {
+        console.log('User is validated')
+    }
+
+    next();
 };
 
 function validatePost(req, res, next) {
+    console.log("running")
+    const text = req.body.text;
 
+    if(!req.body) {
+        res.status(400).json({errorMessage: "Missing post data"})
+    } else if(!text) {
+        res.status(400).json({errorMessage: 'Missing required text field'})
+    } else {
+        console.log('Post is valid')
+    }
+    
+    next();
 };
 
 module.exports = router;
