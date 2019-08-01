eac59b9ac04c56163425339c72d7564b1f09f126
Pherpher089 <Christutor089@gmail.com>
2019-08-01 03:28:05 -0700

    Added in the route handelers for /api/post/ as well as the validateId middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -15,22 +15,66 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, async (req, res) => {
+    const {id} = req.params;
+
+    if(!id) {
+        res.status(401).json({errorMessage: "Please provide a user id in the request path."})
+    }
+
+    try {
+        const posts = await db.getById(id);
+        res.status(200).json(posts);
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, async (req, res) => {
+    const {id} = req.params;
 
+    if(!id) {
+        res.status(401).json({errorMessage: "Please provide a post id in the request path."})
+    }
+
+    try {
+        const deletedPost = await db.remove(id);
+        res.status(200).json(deletedPost);
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, async (req, res) => {
+    const {id} = req.params;
+    const {text} = req.body;
 
+    if(!id) {
+        res.status(401).json({errorMessage: "Please provide a post id in the request path."});
+    } 
+    if(!text) {
+        res.status(401).json({errorMessage: "Please provide the user id and the updated post"});
+    }
+    
+    try {
+        const updatedPost = await db.update(req.params.id, req.body);
+        res.status(201).json(updatedPost);
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const id = db.getById(req.params.id);
 
+    if(!id) {
+        res.status(401).json({errorMessage: "This post ID does not exist"})
+    }
+
+    next()
 };
 
 module.exports = router;
